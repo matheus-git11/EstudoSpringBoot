bcb948b6aa1c4bedd9597def1791289960b9d95b
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-06 01:51:07 -0300

    adicionando classe configuration com Bean

---
+++ src/main/java/io/github/matheusgit11/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.matheusgit11;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de vendas";
+    }
+}
--- src/main/java/io/github/matheusgit11/VendasApplication.java
+++ src/main/java/io/github/matheusgit11/VendasApplication.java
@@ -1,6 +1,8 @@
 package io.github.matheusgit11;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,11 @@ public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class,args);
     }
-
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
     @GetMapping("/hello")
     public String helloWorld(){
-        return "hello world";
+        return applicationName;
     }
 }
