2ed25ad8e3e1d189af96a24c04225c4bcb0716d2
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-08 23:38:26 -0300

    Implementando as clases do modelo

---
+++ src/main/java/io/github/matheusgit11/domain/entity/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.matheusgit11.domain.entity;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
