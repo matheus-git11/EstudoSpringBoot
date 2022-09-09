808dd4c2909952acffd17dff93e5e48a0bf4fa26
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-09 14:57:45 -0300

    Comecando a refatorar a classe cliente para utilizacao do JPA

--- src/main/java/io/github/matheusgit11/domain/entity/Cliente.java
+++ src/main/java/io/github/matheusgit11/domain/entity/Cliente.java
@@ -1,8 +1,21 @@
 package io.github.matheusgit11.domain.entity;
 
+
+
+
+
+import javax.persistence.*;
+
+@Entity                   //considerar essa classe como uma entidade e registrar ela no banco de dados
+@Table(name = "cliente") //table serva se eu usar uma entidade diferente do nome da tabela e tambem outras definicoes , nao necessario para nosso exemplo
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id") // serve para fazer a definicao das colunas do mesmo jeito que a anatocao Table , nao necessario para nosso exemplo mas bom lembrar
     private Integer id;
+
+    @Column(name = "nome" , length = 100)
     private String nome;
 
     public Cliente(){
