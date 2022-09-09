5d16dfca9f4458965773734aafb5fa52458d111e
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-08 23:01:58 -0300

    Conectando spring ao banco h2 e fazendo configuracoes no properties

--- src/main/java/io/github/matheusgit11/Animal.java
+++
@@ -1,5 +0,0 @@
-package io.github.matheusgit11;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/io/github/matheusgit11/VendasApplication.java
+++ src/main/java/io/github/matheusgit11/VendasApplication.java
@@ -1,13 +1,8 @@
 package io.github.matheusgit11;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,25 +12,5 @@ public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
-
-    @Value("${application.name}") //usando o application.properties e nao mais o configuration
-    private String applicationName;
-
-    @Gato
-    private Animal animal;
-    @Cachorro
-    private Animal animal2;
-
-    @Bean(name= "executarAnimal")
-    public CommandLineRunner executar(){
-        return args -> {
-            this.animal.fazerBarulho();
-            this.animal2.fazerBarulho();
-        };
-    }
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
-    }
 }
+
