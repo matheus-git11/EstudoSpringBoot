59e1d9a4f66f9ea3dbc6d3fbb226d40303d8728a
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-15 02:30:15 -0300

    Requisicao get com parametros e ResponseEntity

--- src/main/java/io/github/matheusgit11/VendasApplication.java
+++ src/main/java/io/github/matheusgit11/VendasApplication.java
@@ -1,7 +1,12 @@
 package io.github.matheusgit11;
 
+import io.github.matheusgit11.domain.entity.Cliente;
+import io.github.matheusgit11.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -9,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner (@Autowired Clientes clientes){
+        return args -> {
+            Cliente c = new Cliente(null , "Matheus");
+            clientes.save(c);
+        };
     }
 
 }
--- src/main/java/io/github/matheusgit11/rest/controller/ClienteController.java
+++ src/main/java/io/github/matheusgit11/rest/controller/ClienteController.java
@@ -1,18 +1,29 @@
 package io.github.matheusgit11.rest.controller;
 
+import io.github.matheusgit11.domain.entity.Cliente;
+import io.github.matheusgit11.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes") //definindo a url , toda requisiscao para api clientes vai cair neste controller
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}" , method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) { // o spring injeta esses parametros automaticamento , devido as anotacoes
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s ",nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if(cliente.isPresent()){                       // response entity representa a resposta de uma requisicao
+            return ResponseEntity.ok(cliente.get()); // retorna o resultado da requisiscao , codigo 200 , sucesso
+        }
+        return ResponseEntity.notFound().build();
     }
 }
