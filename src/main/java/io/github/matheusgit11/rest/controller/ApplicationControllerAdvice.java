a49269f3296917b2e51890c7c53e93f2e836cf11
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-20 14:47:06 -0300

    ControllerAdvice e exceptionHandlers para tratar error + devtools

---
+++ src/main/java/io/github/matheusgit11/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package io.github.matheusgit11.rest.controller;
+
+import io.github.matheusgit11.exception.RegraNegocioException;
+import io.github.matheusgit11.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
