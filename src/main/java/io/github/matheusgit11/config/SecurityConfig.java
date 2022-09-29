2e8e9fb72ec7a428899cd6d13d8ad6512289f5a1
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-29 16:27:06 -0300

    Configurando Roles e Authorities

--- src/main/java/io/github/matheusgit11/config/SecurityConfig.java
+++ src/main/java/io/github/matheusgit11/config/SecurityConfig.java
@@ -23,17 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder())
                 .withUser("Fulano")
                 .password(passwordEncoder().encode("123"))
-                .roles("USER");
+                .roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // nao temos uma aplicacao web , estamos trabalhando com rest API
+                .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/clientes/**")
-                        .permitAll()
-                .and()
-                    .formLogin();
+                .antMatchers("/api/clientes/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/api/pedidos/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/api/produtos/**").hasRole("ADMIN")
+                .and().formLogin();
+
     }
 }
