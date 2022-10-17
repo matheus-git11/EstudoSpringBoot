b50b62bfe93f86b191cd363837854b9b6fcf9956
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-10-17 14:44:44 -0300

    Implementando Autorization para o swagger

--- src/main/java/io/github/matheusgit11/config/SwaggerConfig.java
+++ src/main/java/io/github/matheusgit11/config/SwaggerConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,24 +24,50 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("io.github.matheusgit11.rest.controller"))
+                .apis(RequestHandlerSelectors
+                        .basePackage("io.github.matheusgit11.rest.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
-                .title("Vendas Api")
-                .description("Api Restful com base em um projeto de vendas")
+                .title("Vendas API")
+                .description("Api do projeto de Vendas")
                 .version("1.0")
                 .contact(contact())
                 .build();
     }
 
-    private Contact contact(){ // dados de contato do desenvolvedor
+    private Contact contact(){
         return new Contact("Matheus albuquerque"
-                ,"https://github.com/matheus-git11"
-                ,"matheus.2020110153@unicap.br");
+                , "https://github.com/matheus-git11",
+                "matheus.2020110153@unicap.br");
     }
+
+    public ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
+    }
+
 }
